afc77f9fa88aefbe47515ce371676fc85f8fd288
nicohirsau <preussens.nico@gmail.com>
2020-11-09 15:44:08 +0100

    Wrap opcode 0xFNNA in a scope to hinder cross initialization of bool value

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -250,16 +250,18 @@ void CHIP8::EmulateCycle() {
 
                 case 0x000A: // 
                     // Halt until keypress event
-                    bool key_pressed = false;
-                    for (int i = 0; i < 16; i++) {
-                        if (KEYPAD[i] != 0) {
-                            V[(CURRENT_OPCODE & 0x0F00) >> 8] = i;
-                            key_pressed = true;
+                    {
+                        bool key_pressed = false;
+                        for (int i = 0; i < 16; i++) {
+                            if (KEYPAD[i] != 0) {
+                                V[(CURRENT_OPCODE & 0x0F00) >> 8] = i;
+                                key_pressed = true;
+                            }
                         }
+    
+                        // If no key was pressed, do not increment PC
+                        if (!key_pressed) return;
                     }
-
-                    // If no key was pressed, do not increment PC
-                    if (!key_pressed) return;
 
                     PROGRAM_COUNTER += 2;
                     break;
